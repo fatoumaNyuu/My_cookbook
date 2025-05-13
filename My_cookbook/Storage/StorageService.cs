ed60dfc1b11799e8a61f89528417ed7594d72636
fatouma <fatouma1201@gmail.com>
2025-05-13 16:16:52 +0200

    feat: serialization

--- My_cookbook/Storage/StorageService.cs
+++ My_cookbook/Storage/StorageService.cs
@@ -1,8 +1,30 @@
 ﻿using My_cookbook.Models;
-using My_cookbook.Services;
 using System.Text.Json;
 
 namespace My_cookbook.Storage
 {
-    
+    internal static class StorageService
+    {
+        private static string _storagePath = "recipe_storage.json";
+
+        static StorageService()
+        {
+            if (!File.Exists(_storagePath))
+            {
+                File.Create(_storagePath);  // falls es keine Datei gibt, wird im Storage Path eine neue angelegt
+            }
+        }
+
+        public static List<RecipeModel> Load()
+        {
+            var data = File.ReadAllText(_storagePath);
+            return JsonSerializer.Deserialize<List<RecipeModel>>(data) ?? new List<RecipeModel>(); // Serialisierung, falls keine Liste vorhanden ist, wird neuee angelegt!
+        }
+
+        public static void Save(List<RecipeModel> recipeList)
+        {
+            var json = JsonSerializer.Serialize(recipeList, new JsonSerializerOptions { WriteIndented = true });  // Wandelt die Daten in Text um
+            File.WriteAllText(_storagePath, json);
+        }
+    }
 }
--- My_cookbook/obj/Debug/net9.0/My_cookbook.AssemblyInfo.cs
+++ My_cookbook/obj/Debug/net9.0/My_cookbook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("My_cookbook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c19dff595073b5c7c1822a6d089a3a383c6e030")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66e1af8ce3dac4e4e098845f9d9fb7348a62aac7")]
 [assembly: System.Reflection.AssemblyProductAttribute("My_cookbook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("My_cookbook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
